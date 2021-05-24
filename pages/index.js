eeb2d220dc2e69f64aa4c72100c08d8c2a8989d8
David Cullum <davecullum7@gmail.com>
2021-05-24 01:31:42 -0400

    Added Name

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <br /> By: David Cullum
         </h1>
 
         <p className={styles.description}>
